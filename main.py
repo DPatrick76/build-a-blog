f823c68b911d6f886ee116adb71bd282a740f764
DPatrick76 <dougpat76@gmail.com>
2016-09-19 12:56:35 -0500

    Work to demo and submit

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import jinja2
 
 from google.appengine.ext import db  #code for Googel App Engine Database
 
-template_dir = os.path.join(os.path.dirname(__file__), 'template')
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                 autoescape = True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)  # write calls write to not have to type all the time
+        self.response.write(*a, **kw)  # write calls write to not have to type all the time
 
     def render_str(self, template, **params):  #takes a Template name and returns a string of renderd template
         t = jinja_env.get_template(template)
@@ -24,68 +24,68 @@ class BlogData(db.Model):
     body = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
 
-#class MainPage(Handler):  #mapped to slash and writes ASCII Chan to browser
+class MainPage(Handler):  #mapped to slash and writes ASCII Chan to browser
     #def render_front(self, title="", body="", error=""):  #taking variables and passing to template
         #blog_posts = db.GqlQuery("SELECT * FROM BlogData ORDER BY created DESC LIMIT 5")
-
         #self.render("front.html", title=title, body=body, error=error, blog_posts = blog_posts)
 
-    #def get(self):
-        #self.render_front() #draws the blank form
-
-    #def post(self):
-        #title = self.request.get("title")
-        #body = self.request.get("body")
-
-        #if title and body:
-            #b = BlogData(title = title, body = body)
-            #b.put()
-
-            #self.redirect("/")
-        #else:
-            #error = "We need a title and body!"
-            #self.render_front(title, body, error) #renders form with error message
-class Blog(Handler):
-    """Handles requests coming into '/blog'
-        #e.g. build-a-blog/blog"""
     def get(self):
-        blog_posts = db.GqlQuery("SELECT * FROM BlogData ORDER BY created DESC LIMIT 5")
-        self.render("/blog.html",blog_posts = blog_posts)
-
-class NewPost(Handler):
-    """Handles requests coming into '/newpost'
-        #e.g. build-a-blog/newpost"""
-    def render_front(self, title="", body="", error=""):  #taking variables and passing to template
-        blog_posts = db.GqlQuery("SELECT * FROM BlogData ORDER BY created DESC LIMIT 5")
-
-        self.render("newpost.html", title=title, body=body, error=error, blog_posts = blog_posts)
-    def get(self):
-        self.render("/newpost.html")
+        self.render("newpost.html") #draws the blank form
 
     def post(self):
         title = self.request.get("title")
         body = self.request.get("body")
 
         if title and body:
+            #create new BlogData stored in variable b
             b = BlogData(title = title, body = body)
+            #input into the database
             b.put()
-            self.redirect("/blog")
+            self.redirect("/blog/"+str(b.key().id()))
         else:
-            error = "We need a title and body!"
-            self.render_front(title, body, error) #renders form with error message
-class ViewPostHandler(webapp2.RequestHandler):
-    def get(self,id):
-        indiv = get_by_id(int(id))
-        self.write.response()
+            error = "We need a title and a body!"
+            self.render("newpost.html",title=title, body=body, error=error) #renders form with error message
 
-        #if indiv:
-            #self.render("/blog.html", indiv = indiv)
-        #else:
-            #self.response.write("Not working homie")
+class Blog(Handler):
+    """Handles requests coming into '/blog'
+        #e.g. build-a-blog/blog"""
+    def render_blogPosts(self, title="", body="", error=""):
+        blog_posts = db.GqlQuery("SELECT * FROM BlogData ORDER BY created DESC LIMIT 5")
+        self.render("blog.html",title=title, body=body,error=error, blog_posts= blog_posts)
+
+    def get(self):
+        self.render_blogPosts()
+
+    def post(self):
+        a = BlogData(title = title, body = body)
+        b.put()
+
+class ViewPostHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.write(*a, **kw)
+
+    def render_str(self,template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+    def get(self,id):
+        id = int(id)
+        blogEntry = BlogData.get_by_id(id)
+        title = BlogData.title
+        body = BlogData.body
+        blogEntry = [blogEntry]
+
+        if blogEntry:
+            self.render("blog.html", title=title, body=body, blog_posts = blogEntry, oneEntry=True)
+        else:
+            self.response.write("Not working homie.")
 
 app = webapp2.WSGIApplication([
-    #('/', MainPage),
+    ('/', MainPage),
+    ('/newpost', MainPage),
     ('/blog', Blog),
-    ('/newpost', NewPost),
-webapp2.Route('/blog/<id:\d+>',ViewPostHandler)  #put this in the [] brackets
+    webapp2.Route('/blog/<id:\d+>',ViewPostHandler)
 ], debug=True)
